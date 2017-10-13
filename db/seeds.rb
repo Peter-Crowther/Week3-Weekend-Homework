9b066e5219d9c15b7a31dff7e27708272a15d9c5
Peter Crowther <petercrowther90@hotmail.com>
2017-10-13 15:05:32 +0100

    added save, all and delete_all methods to customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,10 @@ require_relative("../models/customer.rb")
 require_relative("../models/film.rb")
 require_relative("../models/ticket.rb")
 
-user_1 = Customer.new({ "name" => "Ronald Ragan"})
+customer_1 = Customer.new({ "name" => "Ronald Ragan", "funds" => 10})
+customer_1.save()
 
 film_1 = Film.new({ "title" => "Armgeddon", "price" => 8 })
+
+binding.pry
+nil
--- models/customer.rb
+++ models/customer.rb
@@ -1,11 +1,41 @@
+require_relative("../db/sql_runner.rb")
+
 class Customer
-attr_accessor :name, :funds
 attr_reader :id
+attr_accessor :name, :funds
+
   def initialize(options)
     @id = options["id"].to_i()
     @name = options["name"]
     @funds = options["funds"].to_i()
   end
+
+    def save()
+      sql = "INSERT INTO customers ( name, funds )
+      VALUES ($1, $2 )
+      RETURNING id
+      "
+      values = [@name, @funds]
+      customer = SqlRunner.run(sql, values).first()
+      @id = customer["id"].to_i()
+    end
+
+    def self.all()
+      sql = "SELECT * FROM customers"
+      values = []
+      results = SqlRunner.run(sql, values)
+      customers = results.map{ |customer| Customer.new(customer)}
+      return customers
+    end
+
+    def self.delete_all()
+      sql = "DELETE FROM customers"
+      values = []
+      SqlRunner.run(sql, values)
+    end
+
+
+
 
 
 
