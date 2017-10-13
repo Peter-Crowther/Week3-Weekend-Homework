4f7473f421e59255b70781d49d1c6bea55418f53
Peter Crowther <petercrowther90@hotmail.com>
2017-10-13 16:56:55 +0100

    created customer and film methods for ticket

--- models/ticket.rb
+++ models/ticket.rb
@@ -21,5 +21,19 @@ attr_accessor :customer_id, :film_id
     @id = SqlRunner.run(sql, values)[0]["id"].to_i()
   end
 
-  
+  def film
+    sql = "SELECT * FROM films
+    WHERE id = $1"
+    values = [@film_id]
+    results = SqlRunner.run(sql, values)
+    return Film.new(results[0])
+  end
+
+  def customer
+    sql = "SELECT * FROM customers
+    WHERE id = $1"
+    values = [@customer_id]
+    results = SqlRunner.run(sql, values)
+    return Customer.new(results[0])
+  end
 end
