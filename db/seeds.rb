574d867d4e426eb19b3c780ca9ad7cf4225c0574
Peter Crowther <petercrowther90@hotmail.com>
2017-10-13 15:59:08 +0100

    added save method to films

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@ require_relative("../models/ticket.rb")
 
 customer_1 = Customer.new({ "name" => "Ronald Ragan", "funds" => 10})
 customer_1.save()
+customer_2 = Customer.new({ "name" => "Ben Benson", "funds" => 30})
+customer_2.save()
+customer_3 = Customer.new({ "name" => "Dawn Chorus", "funds" => 25})
+customer_3.save()
 
 film_1 = Film.new({ "title" => "Armgeddon", "price" => 8 })
+film_1.save()
+film_2 = Film.new({ "title" => "Goldfinger", "price" => 5 })
+film_2.save()
+film_3 = Film.new({ "title" => "Frozen", "price" => 8 })
+film_3.save()
 
 binding.pry
 nil
--- models/film.rb
+++ models/film.rb
@@ -6,4 +6,14 @@ attr_reader :id, :title, :price
     @title = options["title"]
     @price = options["price"].to_i()
   end
+
+  def save()
+    sql = "INSERT INTO films ( title, price )
+          VALUES ( $1, $2 )
+          RETURNING id
+          "
+    values = [@title, @price]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i()
+
+  end
 end
